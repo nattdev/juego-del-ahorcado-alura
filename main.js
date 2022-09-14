25fff4f03250b083027b7cffe0bd4bb3ad1ec690
Vanessa <110750471+nattdev@users.noreply.github.com>
2022-09-13 19:43:52 -0500

    Agregar funcion crearLineasPalabra

--- main.js
+++ main.js
@@ -3,7 +3,17 @@ let listaPalabras = ["primera", "segunda", "tercera", "cuarta"];
 let btnIniciarJuego = document.querySelector("#btn-iniciar-juego");
 let btnAgregarPalabra = document.querySelector("#btn-agregar-palabra");
 
+let boxPalabraOculta = document.querySelector(".palabra-oculta");
+
 let palabraSecreta = "";
+
+function crearLineasPalabra() {
+	for(let i = 0; i < palabraSecreta.length; i++){
+		let espacioLetra = document.createElement("div");
+		boxPalabraOculta.appendChild(espacioLetra);		
+	}
+}
+
 
 function escogerPalabra() {
 	palabraSecreta = aleatorizarPalabra(listaPalabras);
@@ -19,4 +29,17 @@ function aleatorizarPalabra(listaPalabras) {
 	return listaPalabras[Math.floor(Math.random() * listaPalabras.length)];
 }
 
-btnIniciarJuego.addEventListener("click", escogerPalabra);
+function limpiarPalabra() {
+	while(boxPalabraOculta.hasChildNodes()) {
+		boxPalabraOculta.removeChild(boxPalabraOculta.firstChild);
+	}
+}
+
+function iniciarJuego(){	
+	limpiarPalabra();
+	escogerPalabra();
+	crearLineasPalabra();
+}
+
+
+btnIniciarJuego.addEventListener("click", iniciarJuego);
